4de4249c27841719ce2afa8e510e8b94c22406b7
korkmazalibaran36 <korkmazalibaran36@gmail.com>
2024-01-20 20:07:43 +0300

    Min Max eklendi

--- lib/yahoo_finance_data.dart
+++ lib/yahoo_finance_data.dart
@@ -3,6 +3,7 @@
 import 'package:yahoofin/yahoofin.dart';
 
 class PriceController {
+
   Future<List<double>> getPriceOneDay(String ticker) async {
     var yfin = YahooFin();
 
@@ -165,6 +166,340 @@ class PriceController {
     }
 
     List<double> data = chart.chartQuotes!.close!
+        .map((value) => double.parse(value.toStringAsFixed(3)))
+        .toList();
+    return data;
+  }
+
+  Future<List<double>> getMinOneDay(String ticker) async {
+    var yfin = YahooFin();
+
+    StockHistory hist = yfin.initStockHistory(ticker: ticker);
+
+    StockChart chart = await yfin.getChartQuotes(
+      stockHistory: hist,
+      interval: StockInterval.oneMinute,
+      period: StockRange.oneDay,
+    );
+
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.twoMinute,
+        period: StockRange.oneDay,
+      );
+    }
+
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.fiveMinute,
+        period: StockRange.oneDay,
+      );
+    }
+
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.fifteenMinute,
+        period: StockRange.oneDay,
+      );
+    }
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.thirtyMinute,
+        period: StockRange.oneDay,
+      );
+    }
+
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.sixtyMinute,
+        period: StockRange.oneDay,
+      );
+    }
+
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.ninetyMinute,
+        period: StockRange.oneDay,
+      );
+    }
+
+    List<double> data = chart.chartQuotes!.low!
+        .map((value) => double.parse(value.toStringAsFixed(2)))
+        .toList();
+    return data;
+  }
+
+  Future<List<double>> getMinOneMonth(String ticker) async {
+    var yfin = YahooFin();
+
+    StockHistory hist = yfin.initStockHistory(ticker: ticker);
+
+    StockChart chart = await yfin.getChartQuotes(
+      stockHistory: hist,
+      interval: StockInterval.fifteenMinute,
+      period: StockRange.oneMonth,
+    );
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.thirtyMinute,
+        period: StockRange.oneMonth,
+      );
+    }
+
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.sixtyMinute,
+        period: StockRange.oneMonth,
+      );
+    }
+
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.ninetyMinute,
+        period: StockRange.oneMonth,
+      );
+    }
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.oneDay,
+        period: StockRange.oneMonth,
+      );
+    }
+
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.fiveDay,
+        period: StockRange.oneMonth,
+      );
+    }
+
+    List<double> data = chart.chartQuotes!.low!
+        .map((value) => double.parse(value.toStringAsFixed(3)))
+        .toList();
+    return data;
+  }
+
+  Future<List<double>> getMinOneYear(String ticker) async {
+    var yfin = YahooFin();
+
+    StockHistory hist = yfin.initStockHistory(ticker: ticker);
+
+    StockChart chart = await yfin.getChartQuotes(
+      stockHistory: hist,
+      interval: StockInterval.oneDay,
+      period: StockRange.oneYear,
+    );
+
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.fiveDay,
+        period: StockRange.oneYear,
+      );
+    }
+
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.oneWeek,
+        period: StockRange.oneYear,
+      );
+    }
+
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.oneMonth,
+        period: StockRange.oneYear,
+      );
+    }
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.threeMonth,
+        period: StockRange.oneYear,
+      );
+    }
+
+    List<double> data = chart.chartQuotes!.low!
+        .map((value) => double.parse(value.toStringAsFixed(3)))
+        .toList();
+    return data;
+  }
+
+  Future<List<double>> getMaxOneDay(String ticker) async {
+    var yfin = YahooFin();
+
+    StockHistory hist = yfin.initStockHistory(ticker: ticker);
+
+    StockChart chart = await yfin.getChartQuotes(
+      stockHistory: hist,
+      interval: StockInterval.oneMinute,
+      period: StockRange.oneDay,
+    );
+
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.twoMinute,
+        period: StockRange.oneDay,
+      );
+    }
+
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.fiveMinute,
+        period: StockRange.oneDay,
+      );
+    }
+
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.fifteenMinute,
+        period: StockRange.oneDay,
+      );
+    }
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.thirtyMinute,
+        period: StockRange.oneDay,
+      );
+    }
+
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.sixtyMinute,
+        period: StockRange.oneDay,
+      );
+    }
+
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.ninetyMinute,
+        period: StockRange.oneDay,
+      );
+    }
+
+    List<double> data = chart.chartQuotes!.high!
+        .map((value) => double.parse(value.toStringAsFixed(2)))
+        .toList();
+    return data;
+  }
+
+  Future<List<double>> getMaxOneMonth(String ticker) async {
+    var yfin = YahooFin();
+
+    StockHistory hist = yfin.initStockHistory(ticker: ticker);
+
+    StockChart chart = await yfin.getChartQuotes(
+      stockHistory: hist,
+      interval: StockInterval.fifteenMinute,
+      period: StockRange.oneMonth,
+    );
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.thirtyMinute,
+        period: StockRange.oneMonth,
+      );
+    }
+
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.sixtyMinute,
+        period: StockRange.oneMonth,
+      );
+    }
+
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.ninetyMinute,
+        period: StockRange.oneMonth,
+      );
+    }
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.oneDay,
+        period: StockRange.oneMonth,
+      );
+    }
+
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.fiveDay,
+        period: StockRange.oneMonth,
+      );
+    }
+
+    List<double> data = chart.chartQuotes!.high!
+        .map((value) => double.parse(value.toStringAsFixed(3)))
+        .toList();
+    return data;
+  }
+
+  Future<List<double>> getMaxOneYear(String ticker) async {
+    var yfin = YahooFin();
+
+    StockHistory hist = yfin.initStockHistory(ticker: ticker);
+
+    StockChart chart = await yfin.getChartQuotes(
+      stockHistory: hist,
+      interval: StockInterval.oneDay,
+      period: StockRange.oneYear,
+    );
+
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.fiveDay,
+        period: StockRange.oneYear,
+      );
+    }
+
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.oneWeek,
+        period: StockRange.oneYear,
+      );
+    }
+
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.oneMonth,
+        period: StockRange.oneYear,
+      );
+    }
+    if (chart.chartQuotes == null) {
+      chart = await yfin.getChartQuotes(
+        stockHistory: hist,
+        interval: StockInterval.threeMonth,
+        period: StockRange.oneYear,
+      );
+    }
+
+    List<double> data = chart.chartQuotes!.high!
         .map((value) => double.parse(value.toStringAsFixed(3)))
         .toList();
     return data;
